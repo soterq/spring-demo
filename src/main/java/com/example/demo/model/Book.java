3876024b3edba1b458d16b20d37de1a2df43d68e
VitalieS <VitalieS@gilat.com>
2020-01-06 13:17:26 +0200

    firstCommit

---
+++ src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,78 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String isBn;
+    private String publisher;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
+
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String title, String isBn, String publisher) {
+        this.title = title;
+        this.isBn = isBn;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String isBn, String publisher, Set<Author> authors) {
+        this.title = title;
+        this.isBn = isBn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsBn() {
+        return isBn;
+    }
+
+    public void setIsBn(String isBn) {
+        this.isBn = isBn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
